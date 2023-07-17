f3e3ae097f479d61c67a25b2ffbe3f3dc76e0b3a
Ian_Min <ianjasonmin@gmail.com>
2023-07-17 21:30:45 +0900

    Update labelme2yolo.py

--- labelme2yolo.py
+++ labelme2yolo.py
@@ -96,6 +96,7 @@ class labelme2yolo:
         image_files = []
         print(parent_path)
         for file_path in glob.glob(os.path.join(parent_path, '*')):
+            print(file_path)
             if os.path.isfile(file_path) and any(file_path.lower().endswith(ext) for ext in image_extensions):
                 image_files.append(file_path)
         print(image_files)
