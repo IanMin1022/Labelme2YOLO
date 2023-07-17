05d8ea1dc890f7f44fd38bda56f903697d359d93
Ian_Min <ianjasonmin@gmail.com>
2023-07-17 21:41:53 +0900

    Update labelme2yolo.py

--- labelme2yolo.py
+++ labelme2yolo.py
@@ -97,9 +97,9 @@ class labelme2yolo:
         
         for file_path in glob.glob(os.path.join(parent_path, '*')):
             if os.path.isdir(file_path):
+                print(file_path)
                 if os.path.isfile(file_path) and any(file_path.lower().endswith(ext) for ext in image_extensions):
                     image_files.append(file_path)
-            print(image_files)
         
         for path in path:        
             with open(path, encoding=encoding) as cocojson:
